96ab5ccddb4188d89bf8e956fabeea7e55b4939d
tylee <bpthess@naver.com>
2023-06-05 16:57:25 +0900

    카카오 sdk url 적용 테스트

--- app/(auth)/kakaoLogin/page.tsx
+++ app/(auth)/kakaoLogin/page.tsx
@@ -1,14 +1,16 @@
 'use client'
 
 import React, { useEffect } from 'react'
-import { KAKAO_AUTH_URL } from '@/app/libs/client/constants/apiKey'
 
-const KakaoAuth = () => {
+import { KAKAO_AUTH_URL } from '@/app/libs/client/constants/apiKey'
+import Loading from '@/app/loading'
+
+const KakaoLogin = () => {
   useEffect(() => {
     window.location.href = KAKAO_AUTH_URL
   }, [])
 
-  return <></>
+  return <Loading />
 }
 
-export default KakaoAuth
+export default KakaoLogin
--- app/(auth)/oauth/kakao/page.tsx
+++ app/(auth)/oauth/kakao/page.tsx
@@ -1,23 +1,23 @@
 'use client'
 
 import React, { useEffect } from 'react'
-import { useRouter } from 'next/navigation'
+import { useRouter, useSearchParams } from 'next/navigation'
 import { useMutation } from '@tanstack/react-query'
 import { ApiError } from 'next/dist/server/api-utils'
 
 import { kakaoLogin } from '@/app/apis/domain/auth/auth'
 import { PassLoginForm } from '@/app/apis/types/domain/auth/auth'
+import Loading from '@/app/loading'
 
 const KAKAO_TOKEN_URL = 'https://kauth.kakao.com/oauth/token'
 
 const KakaoCallback = () => {
   const router = useRouter()
+  const [serchParams, setSearchParams] = useSearchParams()
+  console.log('serchParams:', serchParams[1])
+
   // 유저정보 스토어에 저장
   // 토큰정보 스토어에 저장
-
-  const params = new URL(window.location.href).searchParams
-  const code = params.get('code')
-  console.log('code:', code)
 
   // 카카오 로그인
   const { mutate: mutateKakaoLogin } = useMutation((params: PassLoginForm) => kakaoLogin(params), {
@@ -59,11 +59,7 @@ const KakaoCallback = () => {
     getKakaoToken()
   }, [])
 
-  return (
-    <>
-      <p>로그인 중입니다.</p>
-    </>
-  )
+  return <Loading />
 }
 
 export default KakaoCallback
