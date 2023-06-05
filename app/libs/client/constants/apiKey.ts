025678bb05d7d66b439644860729321e6f752637
tylee <bpthess@naver.com>
2023-06-05 17:12:12 +0900

    카카오 sdk url 적용 테스트

--- app/libs/client/constants/apiKey.ts
+++ app/libs/client/constants/apiKey.ts
@@ -3,5 +3,5 @@ const REST_API_KEY = '7cb73114517b2b689f5bdca41201b46c'
 // const REDIRECT_URI = 'http://localhost:3000/oauth/kakao'
 
 export const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${REST_API_KEY}&redirect_uri=${
-  window === undefined ? '' : `${window.location.protocol}//${window.location.host}/oauth/kakao`
+  typeof window === 'undefined' ? '' : `${window.location.protocol}//${window.location.host}/oauth/kakao`
 }&response_type=code`
