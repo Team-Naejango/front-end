0f982f8bb495c6fdd5c795d9a0d82bf18bcc4f0a
tylee <bpthess@naver.com>
2023-07-22 08:59:13 +0900

    서버사이드쪽 이슈 제거

--- app/(home)/(main)/warehouse/[...edit]/page.tsx
+++ app/(home)/(main)/warehouse/[...edit]/page.tsx
@@ -1,24 +1,49 @@
 'use client'
 
-import React, { useEffect, useRef } from 'react'
+import React, { useCallback, useEffect } from 'react'
 import { useSearchParams } from 'next/navigation'
-// import _ from 'underscore'
 
 import BackHeader from '@/app/components/template/main/header/BackHeader'
 import { CRUD } from '@/app/libs/client/constants/code'
 
 const WarehouseEdit = () => {
   const searchParam = useSearchParams()
-  const editState = searchParam.get('CRUD')
-  const seqState = searchParam.get('seq')
-  const edit = (editState === CRUD.수정 && seqState !== '') || false
 
-  console.log('editState:', editState)
+  const getEditStatus = useCallback(() => {
+    const editState = searchParam.get('CRUD')
+    const seqState = searchParam.get('seq')
+    const edit = (editState === CRUD.수정 && seqState !== '') || false
+    return { edit }
+  }, [searchParam])
+
+  const { edit } = getEditStatus()
+
+  useEffect(() => {
+    getEditStatus()
+  }, [getEditStatus])
+
+  // console.log('editState:', editState)
 
   // useEffect(() => {
   //   if (edit) {
   //   }
   // }, [editState])
+
+  // useEffect(() => {
+  //   const getCRUDValue = async () => {
+  //     const CRUDValue = searchParam.get('CRUD')
+  //
+  //     if (CRUDValue === CRUD) {
+  //       // 수정 페이지로 처리하는 로직
+  //       console.log('This is the edit page.')
+  //     } else {
+  //       // 생성 페이지로 처리하는 로직
+  //       console.log('This is the create page.')
+  //     }
+  //   }
+  //
+  //   getCRUDValue()
+  // }, [searchParam, CRUD])
 
   return (
     <>
