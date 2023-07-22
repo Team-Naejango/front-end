2a50ff9ef2a47572c8e3697d919bd4d713941bdb
tylee <bpthess@naver.com>
2023-07-22 09:22:56 +0900

    쿼리 파라미터 이슈 제거

--- app/(home)/(main)/warehouse/[...edit]/page.tsx
+++ app/(home)/(main)/warehouse/[...edit]/page.tsx
@@ -8,19 +8,37 @@ import { CRUD } from '@/app/libs/client/constants/code'
 
 const WarehouseEdit = () => {
   const searchParam = useSearchParams()
-
-  const getEditStatus = useCallback(() => {
-    const editState = searchParam.get('CRUD')
-    const seqState = searchParam.get('seq')
-    const edit = (editState === CRUD.수정 && seqState !== '') || false
-    return { edit }
-  }, [])
-
-  const { edit } = getEditStatus()
+  const editState = searchParam.get('CRUD')
+  // const getEditStatus = useCallback(() => {
+  //   const editState = searchParam.get('CRUD')
+  //   const seqState = searchParam.get('seq')
+  //   const edit = (editState === CRUD.수정 && seqState !== '') || false
+  //   return { edit }
+  // }, [])
+  //
+  // const { edit } = getEditStatus()
+  //
+  // useEffect(() => {
+  //   getEditStatus()
+  // }, [])
 
   useEffect(() => {
-    getEditStatus()
-  }, [])
+    const getCRUDValue = async () => {
+      const editState = searchParam.get('CRUD')
+      const seqState = searchParam.get('seq')
+      const edit = (editState === CRUD.수정 && seqState !== '') || false
+
+      if (edit) {
+        // 수정 페이지로 처리하는 로직
+        console.log('This is the edit page.')
+      } else {
+        // 생성 페이지로 처리하는 로직
+        console.log('This is the create page.')
+      }
+    }
+
+    getCRUDValue()
+  }, [searchParam])
 
   // console.log('editState:', editState)
 
@@ -31,8 +49,8 @@ const WarehouseEdit = () => {
 
   return (
     <>
-      <BackHeader canGoBack title={`창고 ${edit ? '편집' : '생성'}`} />
-      <div className={'mt-8'}>{edit ? '편집' : '생성'}</div>
+      <BackHeader canGoBack title={`창고 ${editState === CRUD.수정 ? '편집' : '생성'}`} />
+      <div className={'mt-8'}>{editState === CRUD.수정 ? '편집' : '생성'}</div>
     </>
   )
 }
