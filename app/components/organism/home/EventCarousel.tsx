7cd640b0322d1f31d124243cca85cdcb008e0151
tylee <bpthess@naver.com>
2023-07-23 03:21:28 +0900

    서버사이드 렌더 이슈

--- app/components/organism/home/EventCarousel.tsx
+++ app/components/organism/home/EventCarousel.tsx
@@ -42,7 +42,7 @@ const EventCarousel = () => {
       {homeImagesData.map((data, id) => {
         return (
           <SwiperSlide key={data.title}>
-            <Link href='/events/[id]?id=1'>
+            <Link href={'/events/[id]'} as={`/events/${id}`}>
               <Image
                 src={data.src}
                 alt={'여행 이벤트 이미지'}
