01123a594af39fef438222832e9f56225d32016e
tylee <bpthess@naver.com>
2023-08-07 17:16:51 +0900

    PWA 알림기능 진행중

--- app/page.tsx
+++ app/page.tsx
@@ -22,47 +22,71 @@ const App: NextPage = () => {
     const permission = await Notification.requestPermission()
     if (permission !== 'granted') return
 
-    const firebaseApp = initializeApp({
+    const firebaseApp = {
       apiKey: 'AIzaSyAxyOdpGIONpW9y2xwnskMxvHotpystoK0',
       authDomain: 'naejango-9f387.firebaseapp.com',
       projectId: 'naejango-9f387',
       storageBucket: 'naejango-9f387.appspot.com',
       messagingSenderId: '544393338037',
       appId: '1:544393338037:web:0da3309952fdf3547172fc',
-    })
+    }
 
-    const messaging = getMessaging(firebaseApp)
+    initializeApp(firebaseApp)
+    // const messaging = getMessaging(firebaseApp)
 
     if ('serviceWorker' in navigator) {
       navigator.serviceWorker
         .register('/firebase-messaging-sw.js')
         .then(registration => {
-          console.log('Registration successful, scope is:', registration.scope)
-          getToken(messaging, {
-            vapidKey: 'BG5ChKDaZEdyXKWvbgvqU4rZEmbs0_RtY0cv8aJ5njd6Ks2-DgmMYHg63WPiSg0PrtTR8_9bAVwu78GhHL9GclM',
-            serviceWorkerRegistration: registration,
+          // 서비스 워커 등록 성공
+          console.log('Service Worker 등록 성공')
+
+          // 푸시 알림 권한 요청
+          return registration.pushManager.subscribe({
+            userVisibleOnly: true,
+            applicationServerKey:
+              'BG5ChKDaZEdyXKWvbgvqU4rZEmbs0_RtY0cv8aJ5njd6Ks2-DgmMYHg63WPiSg0PrtTR8_9bAVwu78GhHL9GclM',
           })
-            .then(currentToken => {
-              if (currentToken) {
-                console.log('current token for client: ', currentToken)
-
-                // Track the token -> client mapping, by sending to backend server
-                // show on the UI that permission is secured
-              } else {
-                console.log('No registration token available. Request permission to generate one.')
-
-                // shows on the UI that permission is required
-              }
-            })
-            .catch(err => {
-              console.log('An error occurred while retrieving token. ', err)
-              // catch error while creating client token
-            })
         })
-        .catch(err => {
-          console.log('Service worker registration failed, error:', err)
+        .then(subscription => {
+          // 푸시 알림 구독 성공
+          console.log('푸시 알림 구독 성공', subscription)
+        })
+        .catch(error => {
+          // 오류 처리
+          console.error('Service Worker 등록 및 푸시 알림 구독 오류:', error)
         })
     }
+    // if ('serviceWorker' in navigator) {
+    //   navigator.serviceWorker
+    //     .register('/firebase-messaging-sw.js')
+    //     .then(registration => {
+    //       console.log('Registration successful, scope is:', registration.scope)
+    //       getToken(messaging, {
+    //         vapidKey: 'BG5ChKDaZEdyXKWvbgvqU4rZEmbs0_RtY0cv8aJ5njd6Ks2-DgmMYHg63WPiSg0PrtTR8_9bAVwu78GhHL9GclM',
+    //         serviceWorkerRegistration: registration,
+    //       })
+    //         .then(currentToken => {
+    //           if (currentToken) {
+    //             console.log('current token for client: ', currentToken)
+    //
+    //             // Track the token -> client mapping, by sending to backend server
+    //             // show on the UI that permission is secured
+    //           } else {
+    //             console.log('No registration token available. Request permission to generate one.')
+    //
+    //             // shows on the UI that permission is required
+    //           }
+    //         })
+    //         .catch(err => {
+    //           console.log('An error occurred while retrieving token. ', err)
+    //           // catch error while creating client token
+    //         })
+    //     })
+    //     .catch(err => {
+    //       console.log('Service worker registration failed, error:', err)
+    //     })
+    // }
   }
 
   useEffect(() => {
