9105f755d20f101543969b2fe2e53415bff2ffd3
tylee <bpthess@naver.com>
2023-08-27 19:41:20 +0900

    aws build test

--- app/(routes)/(auth)/(main)/login/page.tsx
+++ app/(routes)/(auth)/(main)/login/page.tsx
@@ -55,13 +55,12 @@ const Login = () => {
 
   const onNonUserLogin = async () => {
     try {
-      // const nonUser = axios.get(`${process.env.NEXT_PUBLIC_API_URL}/api/auth/guest`, {
-      //   withCredentials: true,
-      // })
-      const response = await nonUser()
-      setDeadlineCookie(AUTH_TOKEN.접근, response.data.accessToken)
-      toast.success('비회원 로그인에 성공하였습니다.')
-      router.push('/home')
+      const nonUser = axios.get(`${process.env.NEXT_PUBLIC_API_URL}/api/auth/guest`)
+      nonUser.then(response => {
+        setDeadlineCookie(AUTH_TOKEN.접근, response.data.accessToken)
+        toast.success('비회원 로그인에 성공하였습니다.')
+        router.push('/home')
+      })
     } catch (error: unknown) {
       toast.error('비회원 로그인에 실패하였습니다.')
     }
