0a5017921a7f123b3377162ee4a53d52f923d1cf
tylee <bpthess@naver.com>
2023-08-27 19:45:32 +0900

    aws build test

--- app/(routes)/(auth)/(main)/login/page.tsx
+++ app/(routes)/(auth)/(main)/login/page.tsx
@@ -17,9 +17,6 @@ import { splashState } from '@/app/store/atom'
 import kakaoLogo from '@/app/assets/image/kakao.svg'
 import { setDeadlineCookie } from '@/app/libs/client/utils/cookie'
 import { AUTH_TOKEN } from '@/app/libs/client/constants/store/common'
-import { nonUser } from '@/app/apis/domain/auth/auth'
-
-// const { nonUser } = await import('@/app/apis/domain/auth/auth')
 
 interface FormProps {
   email: string
@@ -55,12 +52,12 @@ const Login = () => {
 
   const onNonUserLogin = async () => {
     try {
-      const nonUser = axios.get(`${process.env.NEXT_PUBLIC_API_URL}/api/auth/guest`)
-      nonUser.then(response => {
-        setDeadlineCookie(AUTH_TOKEN.접근, response.data.accessToken)
-        toast.success('비회원 로그인에 성공하였습니다.')
-        router.push('/home')
+      const response = await axios.get(`${process.env.NEXT_PUBLIC_API_URL}/api/auth/guest`, {
+        withCredentials: true,
       })
+      setDeadlineCookie(AUTH_TOKEN.접근, response.data.accessToken)
+      toast.success('비회원 로그인에 성공하였습니다.')
+      router.push('/home')
     } catch (error: unknown) {
       toast.error('비회원 로그인에 실패하였습니다.')
     }
