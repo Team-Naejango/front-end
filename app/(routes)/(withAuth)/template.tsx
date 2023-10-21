81870607120d68f34c13f1dd50119f663d2647ba
tylee <bpthess@naver.com>
2023-10-21 23:59:01 +0900

    fix: 알림이 종료 시 재연결 시도 강화, 푸쉬알림 json 데이터 예외처리

--- app/(routes)/(withAuth)/template.tsx
+++ app/(routes)/(withAuth)/template.tsx
@@ -17,72 +17,88 @@ import { logout } from '@/app/apis/domain/auth/auth'
 export default function Template({ children }: { children: React.ReactNode }) {
   const router = useRouter()
   const searchParams = useSearchParams()
-
   const accessToken = useRecoilValue<string | undefined>(accessTokenState)
 
   const isLoggedIn = searchParams.get('isLoggedIn') === 'true'
 
-  // 수신 알림 타입
-  const getAlarmStatus = (value: E_NOTIFICATION_TYPE) => {
-    return {
-      TRANSACTION: '거래',
-      CHATTING: '채팅',
-    }[value]
-  }
-
-  // 알림 노출
-  const showNotification = (title: string, content?: string) => {
-    if (Notification.permission === NOTIFICATION_PERMISSION.허용) {
-      const notification = new Notification(title, {
-        body: content,
-      })
-      return notification
-    }
-  }
-
   // 브라우저 알림 구독
-  const subscribe = useCallback(async () => {
-    const EventSource = EventSourcePolyfill || NativeEventSource
-    const SSE = await new EventSource(`${process.env.NEXT_PUBLIC_API_URL}/api/subscribe`, {
-      headers: {
-        Authorization: `Bearer ${accessToken}`,
-      },
-      heartbeatTimeout: 1000 * 60 * 60, // 60분
-      withCredentials: true,
-    })
+  const subscribe = useCallback(
+    async (firstConnection: boolean) => {
+      const EventSource = EventSourcePolyfill || NativeEventSource
+      const SSE = await new EventSource(`${process.env.NEXT_PUBLIC_API_URL}/api/subscribe`, {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+        heartbeatTimeout: 1000 * 60, // 1분
+        withCredentials: true,
+      })
 
-    // 첫 알림 푸시
-    SSE.onopen = () => {
-      if (Notification.permission === NOTIFICATION_PERMISSION.허용) {
-        showNotification('알림', '앱 알림을 구독하였습니다.')
-        toast.success('앱 알림을 구독하였습니다.')
-      }
-    }
-
-    SSE.onerror = () => {
-      SSE.close()
-    }
-
-    // SSE 감지 후 브라우저 알림 노출
-    SSE.addEventListener('sse', (event: any) => {
-      const eventData = JSON.parse(event.data)
-
-      if (eventData) {
-        const alarmInfo: SSEType = {
-          ...eventData,
-        }
-
+      // 알림 노출
+      const showNotification = (title: string, content?: string) => {
         if (Notification.permission === NOTIFICATION_PERMISSION.허용) {
-          showNotification(getAlarmStatus(alarmInfo.notificationType), alarmInfo.content)
-          toast.success(`${alarmInfo.content}`)
+          const notification = new Notification(title, {
+            body: content,
+          })
+          return notification
         }
       }
-    })
 
-    return () => {
-      SSE.close()
-    }
-  }, [EventSourcePolyfill, NativeEventSource])
+      // 첫 알림 푸시
+      if (firstConnection) {
+        SSE.onopen = () => {
+          if (Notification.permission === NOTIFICATION_PERMISSION.허용) {
+            showNotification('알림', '앱 알림을 구독하였습니다.')
+            toast.success('앱 알림을 구독하였습니다.')
+          }
+        }
+      }
+
+      SSE.onerror = () => {
+        SSE.close()
+        subscribe(false)
+      }
+
+      // SSE 감지 후 브라우저 알림 노출
+      SSE.addEventListener('sse', (event: any) => {
+        const isJson = (data: string) => {
+          try {
+            const json = JSON.parse(data)
+            return json && typeof json === 'object'
+          } catch (error: unknown) {
+            return false
+          }
+        }
+
+        if (isJson(event.data)) {
+          const eventData = JSON.parse(event.data)
+
+          if (eventData) {
+            const alarmInfo: SSEType = {
+              ...eventData,
+            }
+
+            // 수신 알림 타입
+            const getAlarmStatus = (value: E_NOTIFICATION_TYPE) => {
+              return {
+                TRANSACTION: '거래',
+                CHATTING: '채팅',
+              }[value]
+            }
+
+            if (Notification.permission === NOTIFICATION_PERMISSION.허용) {
+              showNotification(getAlarmStatus(alarmInfo.notificationType), alarmInfo.content)
+              toast.success(`${alarmInfo.content}`)
+            }
+          }
+        }
+      })
+
+      return () => {
+        SSE.close()
+      }
+    },
+    [EventSourcePolyfill, NativeEventSource]
+  )
 
   // 서비스 워커 등록
   useEffect(() => {
@@ -94,7 +110,7 @@ export default function Template({ children }: { children: React.ReactNode }) {
         if ('serviceWorker' in navigator) {
           navigator.serviceWorker.register('/sw.js').then(async () => {
             try {
-              await subscribe()
+              await subscribe(true)
             } catch (error) {
               if (error instanceof AxiosError) {
                 return Promise.reject(error)
@@ -123,9 +139,9 @@ export default function Template({ children }: { children: React.ReactNode }) {
   }, [isLoggedIn, router])
 
   // 브라우저 종료 시
-  useUnloadEffect(async () => {
-    await logout()
-  })
+  // useUnloadEffect(async () => {
+  //   await logout()
+  // })
 
   return <>{children}</>
 }
