e0142b63e4c2657423593687c41a122be27bfe59
tylee <bpthess@naver.com>
2023-10-31 23:47:43 +0900

    fix: 카카오 로그인 TEMPORAL유저 리다이렉팅 이슈

--- app/page.tsx
+++ app/page.tsx
@@ -1,32 +1,12 @@
 'use client'
 
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 import { NextPage } from 'next'
-import { useRouter } from 'next/navigation'
-import { useRecoilValue } from 'recoil'
 
 import Login from '@/app/(routes)/(auth)/(domain)/login/page'
-import Home from '@/app/(routes)/(withAuth)/(domain)/home/home'
-import { useClearSession } from '@/app/hooks/useClearSession'
-import { accessTokenState } from '@/app/store/auth'
 
 const App: NextPage = () => {
-  const router = useRouter()
-  const { resetToken } = useClearSession()
-  const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false)
-  const accessToken = useRecoilValue<string | undefined>(accessTokenState)
-
-  useEffect(() => {
-    if (accessToken) {
-      setIsLoggedIn(true)
-      router.push('/home')
-    } else {
-      resetToken()
-      router.push('/')
-    }
-  }, [])
-
-  return <>{isLoggedIn ? <Home /> : <Login />}</>
+  return <Login />
 }
 
 export default App
